66903cf22b21636b0f751b9b15bcd68203917366
Kevin Wu <kevinwu098@gmail.com>
2024-02-24 23:56:57 -0800

    fix: remove strict mode for webcam

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    /**
+     * Webcam will turn off if strict mode is enabled
+     * https://medium.com/@kevin0108dsa/react-webcam-starts-webcam-but-stops-automatically-22f006331d83
+     */
+    reactStrictMode: false,
+};
 
 module.exports = nextConfig;
